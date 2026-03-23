e7d4696040c0fd32f72af46250a7815e503f97ac
JWNARI001 <jwnari001@myuct.ac.za>
2026-03-23 16:59:52 +0200

    Created the main class for game that takes input from the user to create a player, and shuffle the deck

--- src/Game.java
+++ src/Game.java
@@ -1,7 +1,21 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Game {
     
-    
+    public static void main (String[] args){
+        
+        Scanner sc = new Scanner(System.in);
+
+        Deck deck = new Deck();
+        deck.shuffle();
+
+        System.out.print("Enter your name: ");
+        String pName = sc.nextLine();
+        Player player = new Player(pName);
+
+        Dealer dealer = new Dealer("Dealer");
+
+    }
 
 }
