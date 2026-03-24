f2b31e9a4a118f1957a296f1a5b554032c4a02a9
JWNARI001 <jwnari001@myuct.ac.za>
2026-03-24 21:51:50 +0200

    Coded the logic begin starting a new game and dealing the player and delaer 2 cards each.

--- src/Game.java
+++ src/Game.java
@@ -16,6 +16,34 @@ public class Game {
 
         Dealer dealer = new Dealer("Dealer");
 
+        player.receiveCard(deck.dealCard());
+        dealer.receiveCard(deck.dealCard());
+        player.receiveCard(deck.dealCard());
+        dealer.receiveCard(deck.dealCard());
+
+        System.out.println(player);
+
+        while (player.getTotal() < 21){
+            System.out.println("Do you want to hit or stand? (h/s)");
+            String decision = sc.nextLine();
+
+            if (decision.equals("h")){
+                player.receiveCard(deck.dealCard());
+                System.out.println(player);
+
+                if (player.getTotal() > 21){
+                    System.out.println("Bust! You went over 21.");
+                    break;
+                }
+
+            }
+            else if (decision.equals("s")){
+                    System.out.println("You stand at " + player.getTotal());
+                    break;    
+            }
+            
+        }
+        
     }
 
 }
